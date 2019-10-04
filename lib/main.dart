80ca69d77466279d4291f4d7ed23a877730fa53f
divoneimacedo <divoneimah@gmail.com>
2019-10-04 12:15:37 -0300

    youtube, layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prinex/toast.dart';
+import 'package:youtube_player/youtube_player.dart';
 void main(){
   runApp(
     MaterialApp(
@@ -24,6 +25,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    TextConst text = TextConst();
     return Scaffold(
       appBar: AppBar(
         title: Image.asset('images/logo_prinex.png',fit: BoxFit.cover),
@@ -32,8 +34,21 @@ class _HomeState extends State<Home> {
 
       ),
       body: Center(child: Container(
+        padding:EdgeInsets.all(5),
         child: Column(
           children: <Widget>[
+            Image.asset("images/1.jpg",fit: BoxFit.fitWidth,),
+            Row(
+              children: <Widget>[
+                Text(text.tituloFacaTeste, style: TextStyle(color:Colors.green,fontWeight: FontWeight.bold,fontSize: 15),)
+              ],
+            ),
+            YoutubePlayer(
+              context: context,
+              source: "zCJLCogqRd",
+              quality: YoutubeQuality.HIGH,
+            ),
+
             RaisedButton(
               child: const Text("Pressione aqui"),
               onPressed: () => _toastEvent(),
@@ -55,11 +70,23 @@ class _HomeState extends State<Home> {
             ),
             ListTile(
               title: Text(
-                  "INICIAL",
-                style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                  "Inicial",
+                style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.lightGreen),
               ),
-              leading: Icon(Icons.home),
+              leading: Icon(Icons.home,color: Colors.lightGreen,),
             ),
+            ListTile(
+              title: Text("Como funciona?",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.lightGreen),),
+              leading: Icon(Icons.question_answer,color: Colors.lightGreen,),
+            ),
+            ListTile(
+              title: Text("Agências",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.lightGreen),),
+              leading: Icon(Icons.local_post_office,color: Colors.lightGreen,),
+            ),
+            ListTile(
+              title: Text("Contato",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.lightGreen),),
+              leading: Icon(Icons.contact_mail,color: Colors.lightGreen,),
+            )
           ],
         ),
       ),
@@ -69,5 +96,15 @@ class _HomeState extends State<Home> {
     Toast.show("Toast plugin app", context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
   }
 
+
+
+
+}
+
+class TextConst{
+  String tituloFacaTeste = "Faca um teste: 0800 42-1000";
+  String textoFacaTeste = "O serviço de encomenda expressa ‘rodoviária a rodoviária’ chegou para suprir a sua necessidade de envio urgente. Sua encomenda é tratada com carinho e viaja nos exclusivos compartimentos Prinex da frota padrão Princesa dos Campos. ";
+  String tituloVvejaComoFunciona = "Veja como funciona";
+  String textoVejaComoFunciona = "Você vai até um dos nossos guichês (nos terminais rodoviários, agências ou unidades de atendimento), escolhe uma de nossas embalagens exclusivas (gratuitas) e sua encomenda é despachada no primeiro ônibus do destino escolhido.";
 }
 
