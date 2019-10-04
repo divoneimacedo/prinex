009864dd34890af1da0fa1b2eb686753c07bb6d0
divoneimacedo <divoneimah@gmail.com>
2019-10-04 14:07:13 -0300

    corrigido youtube, ajustes layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:prinex/toast.dart';
+
+//import 'package:prinex/toast.dart';
+import 'package:toast/toast.dart';
 import 'package:youtube_player/youtube_player.dart';
+
 void main(){
   runApp(
     MaterialApp(
@@ -33,19 +36,32 @@ class _HomeState extends State<Home> {
 
 
       ),
-      body: Center(child: Container(
+      body: SingleChildScrollView(
+        child:Center(child: Container(
         padding:EdgeInsets.all(5),
-        child: Column(
+        child:  Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Image.asset("images/1.jpg",fit: BoxFit.fitWidth,),
-            Row(
-              children: <Widget>[
-                Text(text.tituloFacaTeste, style: TextStyle(color:Colors.green,fontWeight: FontWeight.bold,fontSize: 15),)
-              ],
+            Padding(
+              padding: EdgeInsets.only(top: 5.0,bottom: 5.0),
+              child:Text(text.tituloFacaTeste, style: TextStyle(color:Colors.green,fontWeight: FontWeight.bold,fontSize: 15),textAlign: TextAlign.left),
+            ),
+            Text(text.textoFacaTeste,textAlign: TextAlign.justify,),
+            Padding(
+              padding: EdgeInsets.only(top: 5.0,bottom: 5.0),
+              child: Text(text.tituloVvejaComoFunciona, style: TextStyle(color:Colors.green,fontWeight: FontWeight.bold,fontSize: 15),textAlign: TextAlign.left),
+            ),
+
+            Padding(
+                padding: EdgeInsets.only(top:5.0,bottom: 20.0),
+                child:Text(text.textoVejaComoFunciona,textAlign: TextAlign.justify,)
             ),
             YoutubePlayer(
               context: context,
-              source: "zCJLCogqRd",
+              source: "e3110hCwl6o",
+              autoPlay: false,
               quality: YoutubeQuality.HIGH,
             ),
 
@@ -54,8 +70,11 @@ class _HomeState extends State<Home> {
               onPressed: () => _toastEvent(),
 
             )
+
           ],
+
         ),
+      ),
       ),
       ),
       drawer: Drawer(
@@ -74,6 +93,9 @@ class _HomeState extends State<Home> {
                 style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.lightGreen),
               ),
               leading: Icon(Icons.home,color: Colors.lightGreen,),
+              onTap: (){
+                Toast.show("Inicial", context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+              },
             ),
             ListTile(
               title: Text("Como funciona?",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.lightGreen),),
@@ -92,8 +114,8 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-  void _toastEvent(){
-    Toast.show("Toast plugin app", context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+  void _toastEvent(String message){
+    Toast.show(message, context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
   }
 
 
